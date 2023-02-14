485d295f49d2ac095a356162c685332d7d06d2ef
matukda <103544046+makimacode@users.noreply.github.com>
2023-02-14 02:14:58 -0300

    update js

---
+++ script.js
@@ -0,0 +1,66 @@
+//* Nav Menu
+
+const sideMenu = document.getElementById("side-menu");
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-50%";
+}
+
+//* Language Toggler
+
+// const check = document.querySelector(".toggler");
+
+// check.addEventListener("click",language);
+
+// function language(){
+    
+//     let id = check.checked;
+
+//     if (id == true){
+//         location.href = "es/index.html";
+//     }else{
+//         location.href = "../index.html"
+//     }
+// }
+
+//* Load More
+
+
+
+//* Scroll Reveal
+
+ScrollReveal().reveal('nav');
+ScrollReveal().reveal('#header', { delay: 500 });
+ScrollReveal().reveal('.header-text', { delay: 1000 });
+ScrollReveal().reveal('#about', { delay: 500 });
+ScrollReveal().reveal('.about-col-1', { delay: 1000 });
+ScrollReveal().reveal('.about-col-2', { delay: 1500 });
+ScrollReveal().reveal('#services', { delay: 500 });
+ScrollReveal().reveal('.services-list', { delay: 1000 });
+ScrollReveal().reveal('#portfolio', { delay: 500 });
+ScrollReveal().reveal('.work-list', { delay: 1000 });
+ScrollReveal().reveal('#contact', { delay: 500 });
+ScrollReveal().reveal('.contact-left', { delay: 1000 });
+ScrollReveal().reveal('.contact-right', { delay: 1500 });
+
+
+// const modeToggler = document.getElementById("modeToggler");
+
+// modeToggler.addEventListener("change", function() {
+//     document.body.classList.toggle("dark-mode");
+// });
+
+const modeToggler = document.getElementById("modeToggler");
+
+modeToggler.addEventListener("change", function() {
+    document.body.classList.toggle("dark-mode");
+    if (!modeToggler.checked) {
+        document.body.classList.add("light-mode");
+    } else {
+        document.body.classList.remove("light-mode");
+    }
+});
